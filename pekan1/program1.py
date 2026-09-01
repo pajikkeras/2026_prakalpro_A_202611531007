dd3559c2d43eb94228c555a1aeb0d560c84fa4f7
prak1 <rasyafahrozi06@gmail.com>
2026-09-01 10:02:43 +0700

    pratikum A selasa

---
+++ pekan1/program1.py
@@ -0,0 +1,14 @@
+# Program Pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+
+print("Hello World")
+print()
+print("Progrram ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n"
+      + "Muhammad Rasya Fahrozi dan 2611531007.")
