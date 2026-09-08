2330e18f9274fb70c55d44309279ad5013d382bc
rsyafhrozi <159417096+pajikkeras@users.noreply.github.com>
2026-09-08 09:55:35 +0700

    Delete pekan2/Numeric1_2611531007.py

--- pekan2/Numeric1_2611531007.py
+++
@@ -1,8 +0,0 @@
-a_1007 = 100
-print("The type of variable having value", a_1007, " is", type (a_1007))
-      
-b_1007 = 10.2345
-print("The type of varible having value", b_1007, " is", type (b_1007))  
-
-c_1007 = 100+3j
-print("The type of variable having value", c_1007, " is", type (c_1007))
